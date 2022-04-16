4e1b8432df546adefd86e9bd7d22e7ff77be9da6
patel1078 <jinal.patel2424@gmail.com>
2022-04-16 18:59:29 -0400

    showing current Temp

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "e86684ffe03532fbcbd2b2a744b47023";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=imperial`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
