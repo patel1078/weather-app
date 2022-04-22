03cb12ed52c378e54b2aa39dbf2754a340a7ac6c
patel1078 <jinal.patel2424@gmail.com>
2022-04-22 18:23:53 -0400

    added max and min temp for forecast data

--- src/app.js
+++ src/app.js
@@ -52,12 +52,12 @@ function displayForecast(response) {
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${Math.round(
+          <span class="weather-forecast-temperature-max"> Max: ${Math.round(
             forecastDay.temp.max
-          )}° </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
+          )}°F </span>
+          <span class="weather-forecast-temperature-min"> Min: ${Math.round(
             forecastDay.temp.min
-          )}° </span>
+          )}°F </span>
         </div>
       </div>
   `;
@@ -66,13 +66,11 @@ function displayForecast(response) {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "e86684ffe03532fbcbd2b2a744b47023";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
